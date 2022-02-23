c90452cf685f86430ca955a1d4c2957a6fb34ae4
Emmanuel Gomez <emmagomez166@gmail.com>
2022-02-23 14:32:01 -0300

    Chores
    Styles changed
    Photos chenged
    Page translated

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const { AOS } = require("aos");
-
-AOS.init();
-
-
 const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
 
 
@@ -48,7 +43,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
 
     const shoppingCartRow = document.createElement('div');
     const shoppingCartContent = `
-                <div class="row shoppingCartItem">
+                <div class="row-own shoppingCartItem">
                     <div class="col-6">
                         <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                             <img src=${itemImage} class="shopping-cart-image">
@@ -119,3 +114,5 @@ function buyButtonClicked() {
     shoppingCartItemsContainer.innerHTML = '';
     updateShoppingCartTotal();
 }
+
+
